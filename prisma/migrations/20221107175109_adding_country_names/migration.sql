3ff1b494ec564971a3ffdabf3e7fd480beb86c53
Giovani Carnaval <gi.carnaval91@gmail.com>
2022-11-09 12:43:25 -0300

    commit inicial

---
+++ prisma/migrations/20221107175109_adding_country_names/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE "Game" ADD COLUMN "firstTeamCountryName" TEXT;
+ALTER TABLE "Game" ADD COLUMN "secondTeamCountryName" TEXT;
---
+++ src/server.ts
@@ -0,0 +1,41 @@
+import Fastify from 'fastify'
+
+import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
+
+import { poolRoutes } from './routes/pool'
+import { userRoutes } from './routes/user'
+import { guessRoutes } from './routes/guess'
+import { authRoutes } from './routes/auth'
+import { gameRoutes } from './routes/game'
+
+const start = async () => {
+    const fastify = Fastify({
+        logger: true,
+    })
+
+    try{
+        await fastify.register(cors, {
+            origin: true
+        })
+
+        await fastify.register(jwt, {
+            secret: String(process.env.JWT_SECRET)
+        })
+
+        await fastify.register(authRoutes)
+        await fastify.register(gameRoutes)
+        await fastify.register(guessRoutes)
+        await fastify.register(poolRoutes)
+        await fastify.register(userRoutes)
+
+        await fastify.listen({ port: 3333, host: '0.0.0.0' })
+        
+    } catch (err) {
+        fastify.log.error(err)
+
+        process.exit(1)
+    }
+}
+
+start()
